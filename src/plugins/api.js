96a214351272a93eb76a193bb67d7b145d1b5264
bananinhamj <viliczinskimj@gmail.com>
2023-12-08 10:36:10 -0300

    api

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,20 +1,7 @@
 import axios from 'axios'
-const MY_IP = import.meta.env.VITE_MY_IP
-import { useAuthStore } from '../stores/auth'
 
 const api = axios.create({
-  baseURL: `http://${MY_IP}:19003/api/`
-  // baseURL: 'http://192.168.0.17:19003/api/',
-})
-
-api.interceptors.request.use((config) => {
-  const authStore = useAuthStore()
-  const { token } = authStore
-  console.log(token)
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
+  baseURL: `https://projint-mercado-backend-dev-hbqd.4.us-1.fl0.io`
 })
 
 export default api
